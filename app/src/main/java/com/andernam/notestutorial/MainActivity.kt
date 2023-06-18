4755da334651b4e9ce1925810061a3cf8b515d66
CMETAHKAforURmom <isyperandrei@gmail.com>
2023-06-18 18:13:54 +0700

    Final making

--- app/src/main/java/com/andernam/notestutorial/MainActivity.kt
+++ app/src/main/java/com/andernam/notestutorial/MainActivity.kt
@@ -3,6 +3,16 @@ package com.andernam.notestutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.andernam.notestutorial.feauture_note.presentation.add_edit_notes.AddEditNoteScreen
+import com.andernam.notestutorial.feauture_note.presentation.notes.NotesScreen
+import com.andernam.notestutorial.feauture_note.presentation.utill.Screen
 import com.andernam.notestutorial.ui.theme.NotesTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +20,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotesTutorialTheme {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route
+                    ){
+                        composable(Screen.NotesScreen.route){
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route + "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                            navArgument(
+                                name = "noteId"
+                            ){
+                                type = NavType.IntType
+                                defaultValue = -1
+                            },
+                        )){
 
+                        }
+
+                    }
+                }
             }
         }
     }
